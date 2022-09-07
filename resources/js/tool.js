06c2b6b9e72e22eb3457e7eb55ddefc4422de975
Mior Muhammad Zaki <crynobone@gmail.com>
2022-09-07 14:54:58 +0800

    Add ability to listen to theme switched event.

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,1 +1,17 @@
 window.LaravelNova = require('laravel-nova')
+
+Nova.booting(app => {
+  new MutationObserver(() => {
+    const element = document.documentElement.classList
+    const theme = element.contains('dark') ? 'light' : 'dark'
+
+    Nova.$emit('package: theme-switched', {
+      theme,
+      element
+    })
+  }).observe(document.documentElement, {
+    attributes: true,
+    attributeOldValue: true,
+    attributeFilter: ['class'],
+  })
+})
