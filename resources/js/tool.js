b456dcb0989ca491e17d0cefc0475822ba6c9224
Mior Muhammad Zaki <crynobone@gmail.com>
2022-09-13 10:06:43 +0800

    Only emit `nova.theme.switched` on changes. (#8)
    * Only emit `nova.theme.switched` only on changes.
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Update Assets
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    Co-authored-by: crynobone <crynobone@users.noreply.github.com>

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,14 +1,20 @@
 window.LaravelNova = require('laravel-nova')
 
 Nova.booting(() => {
+  let currentTheme = null
+
   new MutationObserver(() => {
     const element = document.documentElement.classList
     const theme = element.contains('dark') ? 'dark' : 'light'
 
-    Nova.$emit('nova-theme-switched', {
-      theme,
-      element
-    })
+    if (theme !== currentTheme) {
+      Nova.$emit('nova-theme-switched', {
+        theme,
+        element
+      })
+
+      currentTheme = theme
+    }
   }).observe(document.documentElement, {
     attributes: true,
     attributeOldValue: true,
