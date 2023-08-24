e94781c622be22a53bcce76ba74ab0a52ae8b818
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-24 15:08:38 +0800

    Workbench Integration
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Listeners/InstallingWorkbench.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace NovaKit\NovaPackagesTool\Listeners;
+
+use Illuminate\Filesystem\Filesystem;
+use Orchestra\Workbench\Events\InstallStarted;
+use Orchestra\Workbench\Workbench;
+
+class InstallingWorkbench
+{
+    /**
+     * The filesystem instance.
+     *
+     * @var \Illuminate\Filesystem\Filesystem;
+     */
+    public $files;
+
+    /**
+     * Construct a new event listener.
+     */
+    public function __construct(Filesystem $files)
+    {
+        $this->files = $files;
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(InstallStarted $event)
+    {
+        if (! $this->files->exists(Workbench::packagePath('testbench.yaml'))) {
+            $this->files->copy(
+                $workingDirectory.DIRECTORY_SEPARATOR.'testbench.stub',
+                Workbench::packagePath('testbench.yaml')
+            );
+        }
+    }
+}
