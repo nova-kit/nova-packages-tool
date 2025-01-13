389b3402c4e51ae035efed2f9be55671e32d9ba8
Mior Muhammad Zaki <crynobone@gmail.com>
2025-01-13 13:48:16 +0800

    Update LaravelServiceProvider.php

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -3,9 +3,6 @@
 namespace NovaKit\NovaPackagesTool;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Nova\Events\ServingNova;
-use Laravel\Nova\Nova;
-use Laravel\Nova\Script;
 
 class LaravelServiceProvider extends ServiceProvider
 {
